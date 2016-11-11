2072a7b1afdfa6c3edf9e66bb9c43068e9547118
Raphael Chochon <raphaelchochon@live.fr>
2016-11-11 14:00:13 +0100

    Premier graphique de températures sur les dernières 48h fonctionnel

--- graph_48h.php
+++ graph_48h.php
@@ -31,56 +31,84 @@
 			DEBUT SCRIPT HIGHCHARTS
 		-->
 		<script>
-		var data_outTemp = <?php include 'json/temperature_48h.json' ?>
+		eval('var time = <?php include 'json/time_48h.json' ?>');
+		eval('var data_outTemp = <?php include 'json/temperature_48h.json' ?>');
+		eval('var data_hygro = <?php include 'json/hygrometrie_48h.json' ?>');
+
+		function comArr(unitsArray) {
+			var outarr = [];
+			for (var i = 0; i < time.length; i++) {
+				outarr[i] = [time[i], unitsArray[i]];
+			}
+			return outarr;
+		}
+
+
+
 
 			$(function () {
+				Highcharts.setOptions({
+					global: {
+						useUTC: false
+					},
+					lang: {
+						months: ["Janvier "," Février "," Mars "," Avril "," Mai "," Juin "," Juillet "," Août "," Septembre "," Octobre "," Novembre "," Décembre"],
+						weekdays: ["Dim "," Lun "," Mar "," Mer "," Jeu "," Ven "," Sam"],
+						shortMonths: ['Jan', 'Fev', 'Mar', 'Avr', 'Mai', 'Juin', 'Juil','Août', 'Sept', 'Oct', 'Nov', 'Déc'],
+						contextButtonTitle: "Menu",
+						decimalPoint: ',',
+						resetZoom: 'Reset zoom',
+						resetZoomTitle: 'Reset zoom à 1:1',
+						downloadPNG: "Télécharger au format PNG image",
+						downloadJPEG: "Télécharger au format JPEG image",
+						downloadPDF: "Télécharger au format PDF document",
+						downloadSVG: "Télécharger au format SVG vector image",
+						printChart: "Imprimer le graphique",
+						loading: "Chargement..."
+					}
+				});
+/*
+	START GRAPH
+ */
 				var temperature = Highcharts.chart ('graph_temp', {
 					chart: {
-						type : 'line'
+						type : 'line',
+						zoomType: 'x',
+						//alignTicks: false,
 					},
 					title: {
-						text: 'Température',
+						text: 'Température et humidité des dernières 48 heures',
 						x: -20 //center
 					},
 					subtitle: {
-						text: 'Source: WorldClimate.com',
+						text: 'Station <?php echo $station_name; ?>',
 						x: -20
 					},
+					credits: {
+						text: 'Association Nice Météo 06',
+						href: 'http://nicemeteo.fr'
+					},
 					xAxis: {
-						categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-							'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+						type: 'datetime',
+						//categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 					},
 					yAxis: {
 						title: {
-							text: 'Temperature (°C)'
+							text: 'Température (°C)'
 						},
 						plotLines: [{
 							value: 0,
 							width: 1,
-							color: '#808080'
+							color: '#FF0000'
 						}]
 					},
 					tooltip: {
 						valueSuffix: '°C'
 					},
-					legend: {
-						layout: 'vertical',
-						align: 'right',
-						verticalAlign: 'middle',
-						borderWidth: 0
-					},
 					series: [{
-						name: 'Tokyo',
-						data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
-					}, {
-						name: 'New York',
-						data: [-0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5]
-					}, {
-						name: 'Berlin',
-						data: [-0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9, 1.0]
-					}, {
-						name: 'London',
-						data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
+						name: 'Température',
+						type: 'spline',
+						data: comArr(data_outTemp)
 					}]
 				});
 			});
