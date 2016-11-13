455dfab554a18868fdd0a0836521ca950b1bc06b
Raphael Chochon <raphaelchochon@live.fr>
2016-11-13 16:34:44 +0100

    Suppression du lien de contact dans la navbar

--- nav.php
+++ nav.php
@@ -44,9 +44,6 @@
 				</li>
 				<li><a href="a-propos.php">A propos</a></li>
 			</ul>
-			<ul class="nav navbar-nav navbar-right">
-				<li><a href="#">Contact</a></li>
-			</ul>
 		</div>
 	</div>
 </nav>
