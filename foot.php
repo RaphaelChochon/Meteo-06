fbe83486c6a7b9946bac7add8ab2eefeea484b77
Raphael Chochon <raphaelchochon@live.fr>
2016-11-13 17:02:04 +0100

    Ajout du site de l'asso dans le footer

--- foot.php
+++ foot.php
@@ -1,8 +1,8 @@
 <div class="row">
 	<div class="col-md-12" align="center">
 		<div class="well">
-			<p><a href="mentions-leg.php"> Mentions légales et réserves de responsabilités</a><br>
-			Maintenance de la station par <?php echo $name_manager_footer; ?><br>
+			<p>Maintenance de la station par <a href="<?php echo $site_manager_graph; ?>" target="blank"><?php echo $name_manager_footer; ?></a><br>
+			<a href="mentions-leg.php"> Mentions légales et réserves de responsabilités</a><br>
 			Pour nous contacter : <?php echo $contact_mail_footer; ?><br>
 			Modèle de station : <?php echo $station_model; ?><br>
 			Version du site : <?php echo $version_site; ?></p>
