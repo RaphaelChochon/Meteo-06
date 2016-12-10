82d1cd78c87407a7140773b5ace7e27f7041e317
Raphael Chochon <raphaelchochon@live.fr>
2016-12-10 13:36:57 +0100

    Correctif script d'install

--- install/script_install.sh
+++ install/script_install.sh
@@ -4,4 +4,5 @@ cp -iv config-SAMPLE.php config.php
 cp -iv json_cron-SAMPLE.sh json_cron.sh
 cp -iv web_analytics-SAMPLE.php web_analytics.php
 cp -iv additional_menu-SAMPLE.php additional_menu.php
+cp -iv res_sociaux-SAMPLE.php res_sociaux.php
 chmod +x json_cron.sh
