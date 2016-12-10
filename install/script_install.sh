8af2f04c1584a4468818a6264683b9ebb593911f
Raphael Chochon <raphaelchochon@live.fr>
2016-12-10 13:33:22 +0100

    Modification du script d'install

--- install/script_install.sh
+++ install/script_install.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 cd ../config/
+cp -iv config-SAMPLE.php config.php
+cp -iv json_cron-SAMPLE.sh json_cron.sh
+cp -iv web_analytics-SAMPLE.php web_analytics.php
+cp -iv additional_menu-SAMPLE.php additional_menu.php
 chmod +x json_cron.sh
