fad003bf8cc81e8283824f3a1c7980f2e7bdb35c
Raphael Chochon <raphaelchochon@live.fr>
2017-01-06 14:07:41 +0100

    Ajout de la station de Valbonne dans le menu additionnel

--- config/additional_menu-SAMPLE.php
+++ config/additional_menu-SAMPLE.php
@@ -2,6 +2,7 @@
 	<ul class="dropdown-menu" role="menu">
 		<li><a href="http://meteo06.fr" target="blank">Portail Météo 06</a></li>
 		<li class="divider"></li>
+		<li><a href="http://valbonne.meteo06.fr" target="blank">Valbonne</a></li>
 		<li><a href="http://cagnes.meteo06.fr" target="blank">Cagnes/Mer</a></li>
 		<li><a href="http://nice.meteo06.fr" target="blank">Nice-Pessicart</a></li>
 		<li><a href="http://gve-meteo-carlone.unice.fr/" target="blank">Nice-Carlone</a></li>
