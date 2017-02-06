fc0ae47fbf2a5f92ad965300a8da751f4b31de80
Raphael Chochon <raphaelchochon@live.fr>
2017-02-06 14:58:48 +0100

    Modification du menu additionnel

--- config/additional_menu-SAMPLE.php
+++ config/additional_menu-SAMPLE.php
@@ -1,6 +1,6 @@
 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Réseau de stations<span class="caret"></span></a>
 	<ul class="dropdown-menu" role="menu">
-		<li><a href="http://meteo06.fr" target="blank">Portail Météo 06</a></li>
+		<li><a href="http://meteo06.fr" target="blank">Portail/Site Météo 06</a></li>
 		<li class="divider"></li>
 		<li><a href="http://valbonne.meteo06.fr" target="blank">Valbonne</a></li>
 		<li><a href="http://cagnes.meteo06.fr" target="blank">Cagnes/Mer</a></li>
@@ -10,8 +10,6 @@
 		<li><a href="http://gilette.meteo06.fr" target="blank">Gilette</a></li>
 		<li><a href="http://clans.meteo06.fr" target="blank">Clans</a></li>
 		<li><a href="http://ilonse.meteo06.fr" target="blank">Ilonse</a></li>
-		<li class="divider"></li>
-		<li><a href="http://nicemeteo.fr" target="blank">Site de l'association</a></li>
 		<li class="divider"></li>
 		<li><a href="https://www.facebook.com/NiceMeteo/" target="blank">Facebook</a></li>
 		<li><a href="https://twitter.com/NiceMeteo06" target="blank">Twitter</a></li>
