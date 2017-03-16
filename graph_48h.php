33941578e37cab997646c7e9402d97f99ea1f5d6
Raphael Chochon <raphaelchochon@live.fr>
2017-03-16 20:52:42 +0100

    Suppression de la décimale pour le graph 48h des UV

--- graph_48h.php
+++ graph_48h.php
@@ -748,7 +748,7 @@
 					tooltip: {
 						//shared: true,
 						pointFormat: '<span style="color:{series.color}">{series.name} :</span> <b>{point.y}</b><br/>',
-						valueDecimals: 1,
+						valueDecimals: 0,
 						//valueSuffix: '',
 					},
 					series: [{
