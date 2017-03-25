e0028c1089b0832c24a0ac5514f0876e5da150d8
Raphael Chochon <raphaelchochon@live.fr>
2017-03-25 23:40:03 +0100

    Correctif URLs canoniques

--- interieur.php
+++ interieur.php
@@ -7,6 +7,7 @@
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="canonical" href="//<?php echo $_SERVER['HTTP_HOST']; ?><?php echo $_SERVER['PHP_SELF']; ?>" />
 		<?php include 'config/favicon.php';?>
 		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
 		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
