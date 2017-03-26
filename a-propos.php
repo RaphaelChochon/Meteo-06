090330759f5c2f7e81d37965ab554ae09dba5d83
Raphael Chochon <raphaelchochon@live.fr>
2017-03-26 21:57:46 +0200

    Ajout du carousel Bootstrap pour la page ``a-propos.php``

--- a-propos.php
+++ a-propos.php
@@ -38,6 +38,7 @@
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 		<link href="vendors/bootswatch-flatly/bootstrap.min.css" rel="stylesheet">
 		<link href="vendors/custom/custom.css" rel="stylesheet">
+		<script src="vendors/bootstrap/js/carousel.js"></script>
 		<script src="vendors/bootstrap/js/bootstrap.min.js"></script>
 	</head>
 	<body>
