643c8f0ab5d803332874b8ff1f4898de429129f0
Raphael Chochon <raphaelchochon@live.fr>
2017-03-26 17:36:20 +0200

    Alignement des paramètres du ``config-SAMPLE.php``
    + propre et + facile de s'y retrouver !
    Aucun autres changements

--- config/config-SAMPLE.php
+++ config/config-SAMPLE.php
@@ -22,56 +22,56 @@
 
 
 	// CONNECT BDD MySQL WEEWX
-	$server ="localhost"; //localhost or host adress/IP
-	$user="weewx_for_example"; // User MySQL
-	$pass="passe"; // password
-	$db_name="weewx"; // BDD name
+	$server  ="localhost"; //localhost or host adress/IP
+	$user    ="weewx_for_example"; // User MySQL
+	$pass    ="passe"; // password
+	$db_name ="weewx"; // BDD name
 	$db_table="archive"; // table name
 
 	// GENERAL CONFIG
-	$station_name = "Name of my station";
-	$short_station_name = "Short name";
-	$url_site="https://myweathersite.fr"; // URL of the site WITHOUT the slash at the end ('subdomain.domain.tld' for example). This is important for SEO tools (google referencing for example)
-	$SSL = true; // If the site is in https (SSL on) then it's true, otherwise write false
-	$station_model = "Oregon Scientific WMR200"; //Station model : Oregon Scientific WMR200, WMR88 ; Davis VP2 etc.
-	$station_altitude = "---"; // Elevation/altitude of the station in meters
-	$date_install_station = "01/01/1970"; // Start of the database MySQL Weewx, at format dd/mm/aaaa
+	$station_name        = "Name of my station";
+	$short_station_name  = "Short name";
+	$url_site            ="https://myweathersite.fr"; // URL of the site WITHOUT the slash at the end ('subdomain.domain.tld' for example). This is important for SEO tools (google referencing for example)
+	$SSL                 = true; // If the site is in https (SSL on) then it's true, otherwise write false
+	$station_model       = "Oregon Scientific WMR200"; //Station model : Oregon Scientific WMR200, WMR88 ; Davis VP2 etc.
+	$station_altitude    = "---"; // Elevation/altitude of the station in meters
+	$date_install_station= "01/01/1970"; // Start of the database MySQL Weewx, at format dd/mm/aaaa
 	$name_manager_footer = "l'association Nice Météo 06"; // Manager name or association maintaining station
-	$name_manager_graph = "Association Nice Météo 06"; // Name which appears in the charts
-	$site_manager_graph = "http://nicemeteo06.fr"; // URL of site on click to the name manager in the charts
+	$name_manager_graph  = "Association Nice Météo 06"; // Name which appears in the charts
+	$site_manager_graph  = "http://nicemeteo06.fr"; // URL of site on click to the name manager in the charts
 	$contact_mail_footer = "nice.meteo06[at]example.com"; // mail address to contact the station manager
-	$extension_logo = "jpg"; // Logo extension format : "png" or "jpg" or "jpeg" etc.
+	$extension_logo      = "jpg"; // Logo extension format : "png" or "jpg" or "jpeg" etc.
 
 	// WEBCAM
-	$presence_webcam = true; // if true change header title and menu
-	$webcam_url_1 = "webcam/lastsnap.jpg"; // URL or path of webcam 1 jpeg, png, etc
-	$webcam_view_1 = "Vue sur Nice - Plein Sud"; // Viewing angle of the webcam 2. ex Vue sur Nice - Plein Sud
+	$presence_webcam       = true; // if true change header title and menu
+	$webcam_url_1          = "webcam/lastsnap.jpg"; // URL or path of webcam 1 jpeg, png, etc
+	$webcam_view_1         = "Vue sur Nice - Plein Sud"; // Viewing angle of the webcam 2. ex Vue sur Nice - Plein Sud
 
-	$presence_second_webcam = false; // if true, display a second webcam on page webcam.php
+	$presence_second_webcam= false; // if true, display a second webcam on page webcam.php
 									//(require $presence_webcam true)
-	$webcam_url_2 = "webcam/lastsnap2.jpg"; // URL or path of webcam 2 jpeg, png, etc
-	$webcam_view_2 = "Plein Nord"; // Viewing angle of the webcam 2. ex Plein Nord
+	$webcam_url_2          = "webcam/lastsnap2.jpg"; // URL or path of webcam 2 jpeg, png, etc
+	$webcam_view_2         = "Plein Nord"; // Viewing angle of the webcam 2. ex Plein Nord
 
-	$webcam_refresh_1 = "5"; // refresh rate of webam in minutes
-	$webcam_refresh_2 = $webcam_refresh_1; //Keep this value if both cameras have the same refresh rate
+	$webcam_refresh_1      = "5"; // refresh rate of webam in minutes
+	$webcam_refresh_2      = $webcam_refresh_1; //Keep this value if both cameras have the same refresh rate
 
 	// TIMELAPSE
-	$timelapse_poster_url_1 = "timelapse/nice.jpg"; // URL or path of miniature timelapse 1
-	$timelapse_url_1 = "timelapse/nice.webm"; // URL or path timelapse 1
+	$timelapse_poster_url_1   = "timelapse/nice.jpg"; // URL or path of miniature timelapse 1
+	$timelapse_url_1          = "timelapse/nice.webm"; // URL or path timelapse 1
 
-	$presence_second_timelapse = false; //if true, display a second timelapse on page webcam.php
-	$timelapse_poster_url_2 = ""; // URL or path of miniature timelapse 2
-	$timelapse_url_2 = ""; // URL or path timelapse 2
+	$presence_second_timelapse= false; //if true, display a second timelapse on page webcam.php
+	$timelapse_poster_url_2   = ""; // URL or path of miniature timelapse 2
+	$timelapse_url_2          = ""; // URL or path timelapse 2
 
 	// ARCHIVES TIMELAPSES
-	$presence_archive_timelapse = true; // if true, display a button with URL or path to the archives
-	$timelapse_archive_url_1 = "archives/nice/"; // URL or path to archive 1
-	$timelapse_archive_url_2 = ""; // URL or path to archive 2
+	$presence_archive_timelapse= true; // if true, display a button with URL or path to the archives
+	$timelapse_archive_url_1   = "archives/nice/"; // URL or path to archive 1
+	$timelapse_archive_url_2   = ""; // URL or path to archive 2
 
 	// SONDES
-	$presence_uv = true; // If true, display values in the tables and charts
-	$presence_radiation = false; // If true, display values in the tables and charts
-	$timestamp_maj_weewx_3_6_0 = "1477605600"; // Before update Weewx 3.6.0 the calcul of ET is wrong. http://www.timestamp.fr/
+	$presence_uv              = true; // If true, display values in the tables and charts
+	$presence_radiation       = false; // If true, display values in the tables and charts
+	$timestamp_maj_weewx_3_6_0= "1477605600"; // Before update Weewx 3.6.0 the calcul of ET is wrong. http://www.timestamp.fr/
 
 
 	// WEB ANALYTIC
@@ -85,13 +85,13 @@
 
 	// BANNIERE INFO
 	$banniere_info_active = true; // enable banniere
-	$banniere_info_type = "warning"; // change color banniere. Value possible : danger/warning/success/info => http://bootswatch.com/flatly/
-	$banniere_info_titre = "Attention !"; // title
-	$banniere_info_message = "Ce site est en travaux, des bugs peuvent encore être présents, soyez donc indulgent.<br>Toute l'équipe vous en remercie";
+	$banniere_info_type   = "warning"; // change color banniere. Value possible : danger/warning/success/info => http://bootswatch.com/flatly/
+	$banniere_info_titre  = "Attention !"; // title
+	$banniere_info_message= "Ce site est en travaux, des bugs peuvent encore être présents, soyez donc indulgent.<br>Toute l'équipe vous en remercie";
 
 	// RADAR
-	$radar_url="//www.infoclimat.fr/api/UzBUfgU%2FBzJWflZgBTwHZFwuBW4AZ1Z%2BAmlbNVV8ADFWPFVgVmUFNAYwBTNTZwloUz5dPAdjV2FTMg%3D%3D/radar/sud_est?4f360c88e3aabaf99aeb8edfecc08542"; // URL of radar
-	$radar_source="InfoClimat"; // source radar
+	$radar_url       ="//www.infoclimat.fr/api/UzBUfgU%2FBzJWflZgBTwHZFwuBW4AZ1Z%2BAmlbNVV8ADFWPFVgVmUFNAYwBTNTZwloUz5dPAdjV2FTMg%3D%3D/radar/sud_est?4f360c88e3aabaf99aeb8edfecc08542"; // URL of radar
+	$radar_source    ="InfoClimat"; // source radar
 	$radar_source_url="http://www.infoclimat.fr/cartes-meteo-temps-reel-images-satellites-infrarouge-visible-haute-resolution.html?i=radar-sud_est"; // URL source radar
 
 	// FACEBOOK
