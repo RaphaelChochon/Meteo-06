e5984828985e8ca69107bbc75dfcec06ee332207
Raphael Chochon <raphaelchochon@live.fr>
2017-07-30 13:30:58 +0200

    Ajout d'une étape de confirmation

--- config/additional_menu-SAMPLE.php
+++ config/additional_menu-SAMPLE.php
@@ -1,15 +1,18 @@
 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Réseau de stations<span class="caret"></span></a>
 	<ul class="dropdown-menu" role="menu">
-		<li><a href="http://meteo06.fr" target="blank">Portail/Site Météo 06</a></li>
+		<li><a href="https://meteo06.fr" target="blank">Portail Météo 06</a></li>
 		<li class="divider"></li>
-		<li><a href="http://valbonne.meteo06.fr" target="blank">Valbonne</a></li>
-		<li><a href="http://cagnes.meteo06.fr" target="blank">Cagnes/Mer</a></li>
-		<li><a href="http://nice.meteo06.fr" target="blank">Nice-Pessicart</a></li>
-		<li><a href="http://gve-meteo-carlone.unice.fr/" target="blank">Nice-Carlone</a></li>
-		<li><a href="http://smdv.meteo06.fr" target="blank">Saint Martin du Var</a></li>
-		<li><a href="http://gilette.meteo06.fr" target="blank">Gilette</a></li>
-		<li><a href="http://clans.meteo06.fr" target="blank">Clans</a></li>
-		<li><a href="http://ilonse.meteo06.fr" target="blank">Ilonse</a></li>
+		<li><a href="https://valbonne.meteo06.fr" target="blank">Valbonne</a></li>
+		<li><a href="https://cagnes.meteo06.fr" target="blank">Cagnes/Mer</a></li>
+		<li><a href="https://vence.meteo06.fr" target="blank">Vence - <span style="color:red;font-weight:bold;">VP2</span></a></li>
+		<li><a href="https://nice-pessicart.meteo06.fr" target="blank">Nice-Pessicart - <span style="color:red;font-weight:bold;">VP2</span></a></li>
+		<li><a href="https://nice-carlone.meteo06.fr" target="blank">Nice-Carlone - <span style="color:red;font-weight:bold;">VP2</span></a></li>
+		<li><a href="https://smdv.meteo06.fr" target="blank">Saint Martin du Var</a></li>
+		<li><a href="https://gilette.meteo06.fr" target="blank">Gilette</a></li>
+		<li><a href="https://clans.meteo06.fr" target="blank">Clans - <span style="color:red;font-weight:bold;">VP2</span></a></li>
+		<li><a href="https://ilonse.meteo06.fr" target="blank">Ilonse - <span style="color:red;font-weight:bold;">VP2</span></a></li>
+		<li class="divider"></li>
+		<li><a href="https://meteo06.fr/presentation-de-lassociation/" target="blank">Association Nice Météo 06</a></li>
 		<li class="divider"></li>
 		<li><a href="https://www.facebook.com/NiceMeteo/" target="blank">Facebook</a></li>
 		<li><a href="https://twitter.com/NiceMeteo06" target="blank">Twitter</a></li>
--- install/script_install.sh
+++ install/script_install.sh
@@ -1,13 +1,37 @@
 #!/bin/bash
-cd ../config/
-cp -iv config-SAMPLE.php config.php
-cp -iv json_cron-SAMPLE.sh json_cron.sh
-cp -iv json_archives_cron-SAMPLE.sh json_archives_cron.sh
-cp -iv web_analytics-SAMPLE.php web_analytics.php
-cp -iv additional_menu-SAMPLE.php additional_menu.php
-cp -iv res_sociaux-SAMPLE.php res_sociaux.php
-cp -iv widget_vigi-SAMPLE.php widget_vigi.php
-cp -iv a-propos-SAMPLE.php a-propos.php
-chmod +x json_cron.sh json_archives_cron.sh
-cd ../img/
-cp -iv logo-SAMPLE.jpg logo.jpg
+
+echo
+echo "== Script avec confirmation =="
+echo
+
+confirm()
+{
+	read -r -p "${1} [y/N] " response
+
+	case "$response" in
+		[yY][eE][sS]|[yY])
+			true
+			;;
+		*)
+			false
+			;;
+	esac
+}
+
+if confirm "Nous allons reconfigurer le site en vous demandant au cas par cas si vous voulez réinitialiser les fichiers de configurations à leur valeur d’origine. Cela aura pour conséquence de perdre vos personnalisations. Vous êtes prêts ?"; then
+	echo "Ok nous allons commencer :"
+	cd ../config/
+	cp -iv config-SAMPLE.php config.php
+	cp -iv json_cron-SAMPLE.sh json_cron.sh
+	cp -iv json_archives_cron-SAMPLE.sh json_archives_cron.sh
+	cp -iv web_analytics-SAMPLE.php web_analytics.php
+	cp -iv additional_menu-SAMPLE.php additional_menu.php
+	cp -iv res_sociaux-SAMPLE.php res_sociaux.php
+	cp -iv widget_vigi-SAMPLE.php widget_vigi.php
+	cp -iv a-propos-SAMPLE.php a-propos.php
+	chmod +x json_cron.sh json_archives_cron.sh
+	cd ../img/
+	cp -iv logo-SAMPLE.jpg logo.jpg
+else
+	echo "Pas grave, nous verrons ça plus tard alors ! ;)"
+fi
