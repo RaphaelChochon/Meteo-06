d89a1483e1c067802e57ed580e12487c291edae4
RaphaelChochon <raphaelchochon@gmail.com>
2018-08-26 20:48:43 +0200

    Ajout de nouvelles stations dans le menu "Réseau de stations"

--- config/additional_menu-SAMPLE.php
+++ config/additional_menu-SAMPLE.php
@@ -2,15 +2,17 @@
 	<ul class="dropdown-menu" role="menu">
 		<li><a href="https://meteo06.fr" target="blank">Portail Météo 06</a></li>
 		<li class="divider"></li>
-		<li><a href="https://valbonne.meteo06.fr" target="blank">Valbonne</a></li>
 		<li><a href="https://cagnes.meteo06.fr" target="blank">Cagnes/Mer</a></li>
-		<li><a href="https://vence.meteo06.fr" target="blank">Vence - <span style="color:red;font-weight:bold;">VP2</span></a></li>
-		<li><a href="https://nice-pessicart.meteo06.fr" target="blank">Nice-Pessicart - <span style="color:red;font-weight:bold;">VP2</span></a></li>
-		<li><a href="https://nice-carlone.meteo06.fr" target="blank">Nice-Carlone - <span style="color:red;font-weight:bold;">VP2</span></a></li>
-		<li><a href="https://smdv.meteo06.fr" target="blank">Saint Martin du Var</a></li>
-		<li><a href="https://gilette.meteo06.fr" target="blank">Gilette</a></li>
+		<li><a href="https://cipieres.meteo06.fr" target="blank">Cipières - <span style="color:red;font-weight:bold;">VP2</span></a></li>
 		<li><a href="https://clans.meteo06.fr" target="blank">Clans - <span style="color:red;font-weight:bold;">VP2</span></a></li>
+		<li><a href="https://gilette.meteo06.fr" target="blank">Gilette</a></li>
 		<li><a href="https://ilonse.meteo06.fr" target="blank">Ilonse - <span style="color:red;font-weight:bold;">VP2</span></a></li>
+		<li><a href="https://nice-carlone.meteo06.fr" target="blank">Nice-Carlone - <span style="color:red;font-weight:bold;">VP2</span></a></li>
+		<li><a href="https://nice-pessicart.meteo06.fr" target="blank">Nice-Pessicart - <span style="color:red;font-weight:bold;">VP2</span></a></li>
+		<li><a href="https://smdv.meteo06.fr" target="blank">Saint Martin du Var</a></li>
+		<li><a href="https://svdt.meteo06.fr/meteo/" target="blank">Saint-Vallier-de-Thiey - <span style="color:red;font-weight:bold;">VP2</span></a></li>
+		<li><a href="https://valbonne.meteo06.fr" target="blank">Valbonne</a></li>
+		<li><a href="https://vence.meteo06.fr" target="blank">Vence - <span style="color:red;font-weight:bold;">VP2</span></a></li>
 		<li class="divider"></li>
 		<li><a href="https://meteo06.fr/presentation-de-lassociation/" target="blank">Association Nice Météo 06</a></li>
 		<li class="divider"></li>
