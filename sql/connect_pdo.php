09da32d7bc67ee8d506dc2445cc4566a829cad8d
Raphaël CHOCHON <raphael@chochon.fr>
2020-04-07 14:41:38 +0200

    Add connect SQL with PDO

---
+++ sql/connect_pdo.php
@@ -0,0 +1,13 @@
+<?php
+	require_once("config/config.php");
+	$db=$db_name.".".$db_table;
+	try {
+		$db_handle_pdo = new PDO("mysql:host=$server;dbname=$db_name", $user, $pass);
+		// Activation des erreurs PDO
+		$db_handle_pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		// mode de fetch par défaut : FETCH_ASSOC / FETCH_OBJ / FETCH_BOTH
+		//$db_handle_pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+	} catch (PDOException $exception) {
+		echo 'Échec lors de la connexion : ' . $exception->getMessage();
+	}
+?>
