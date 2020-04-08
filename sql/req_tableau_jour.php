b8395b11c73a270581d655ce0d90748d94d852db
Raphaël CHOCHON <raphael@chochon.fr>
2020-04-08 12:18:41 +0200

    Déplacement de la fonction wind_cardinals qui était sur la page d'accueil

--- sql/req_tableau_jour.php
+++ sql/req_tableau_jour.php
@@ -134,63 +134,6 @@
 	 * DIRECTION VENT
 	 */
 
-
-	// Dans un premier temps, on définit une fonction qui permettra de calculer
-	// la moyenne de plusieurs angles en degrées
-	function mean_of_angles( $angles, $degrees = true ) {
-		if ( $degrees ) {
-			$angles = array_map("deg2rad", $angles); // Convert to radians
-		}
-		$s_ = 0;
-		$c_ = 0;
-		$len = count( $angles );
-		for ($i = 0; $i < $len; $i++) {
-			$s_ += sin( $angles[$i] );
-			$c_ += cos( $angles[$i] );
-		}
-		// $s_ /= $len;
-		// $c_ /= $len;
-		$mean = atan2( $s_, $c_ );
-		if ( $degrees ) {
-			$mean = rad2deg( $mean ); // Convert to degrees
-		}
-		if ($mean < 0) {
-			$mean_ok = $mean + 360;
-		} else {
-			$mean_ok = $mean;
-		}
-		return $mean_ok;
-	}
-	// Maintenant, on définit une fonction qui servira à nous donner
-	// la position cardinale du vent (en texte plutôt qu'en degrés)
-	function wind_cardinals($deg) {
-		$cardinalDirections = array(
-			'N' => array(348.75, 361),
-			'N2' => array(0, 11.25),
-			'NNE' => array(11.25, 33.75),
-			'NE' => array(33.75, 56.25),
-			'ENE' => array(56.25, 78.75),
-			'E' => array(78.75, 101.25),
-			'ESE' => array(101.25, 123.75),
-			'SE' => array(123.75, 146.25),
-			'SSE' => array(146.25, 168.75),
-			'S' => array(168.75, 191.25),
-			'SSW' => array(191.25, 213.75),
-			'SW' => array(213.75, 236.25),
-			'WSW' => array(236.25, 258.75),
-			'W' => array(258.75, 281.25),
-			'WNW' => array(281.25, 303.75),
-			'NW' => array(303.75, 326.25),
-			'NNW' => array(326.25, 348.75)
-		);
-		foreach ($cardinalDirections as $dir => $angles) {
-			if ($deg >= $angles[0] && $deg < $angles[1]) {
-				$cardinal = str_replace("2", "", $dir);
-			}
-		}
-		return $cardinal;
-	};
-
 	/*
 	 * VENT MOYEN 10 minutes
 	 */
