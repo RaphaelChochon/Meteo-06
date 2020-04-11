f320cc2cf13b956fba988707133e6f6628609af0
Raphaël CHOCHON <raphael@chochon.fr>
2020-04-11 11:40:19 +0200

    Internalisation des libs sur la page des graphiques

--- graphs.php
+++ graphs.php
@@ -109,14 +109,29 @@
 			<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 			<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 		<![endif]-->
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-		<link href="https://code.highcharts.com/css/highcharts.css" rel="stylesheet">
+
+		<!-- JQUERY JS -->
+		<script src="vendors/jquery/jquery-3.4.1.min.js"></script>
+
+		<!-- Bootstrap + custom CSS-->
 		<link href="vendors/bootswatch-flatly/bootstrap.min.css" rel="stylesheet">
 		<link href="vendors/custom/custom.css?v=1.1" rel="stylesheet">
 		<script src="vendors/bootstrap/js/bootstrap.min.js"></script>
-		<script src="https://code.highcharts.com/highcharts.js"></script>
+
+		<!-- Highcharts -->
+		<link href="vendors/highcharts/css/highcharts-8.0.4.css" rel="stylesheet">
+		<script src="vendors/highcharts/js/highcharts-8.0.4.js"></script>
 		<?php if ($graphType == 'heatmap') : ?>
-			<script src="https://code.highcharts.com/modules/heatmap.js"></script>
+			<script src="vendors/highcharts/modules/heatmap-8.0.4.js"></script>
+		<?php endif; ?>
+		<script src="vendors/highcharts/js/highcharts-more-6.2.0.js"></script>
+		<script src="vendors/highcharts/modules/exporting-8.0.4.js"></script>
+		<script src="vendors/highcharts/modules/offline-exporting-8.0.4.js"></script>
+		<script src="vendors/highcharts/modules/export-data-8.0.4.js"></script>
+		<script src="vendors/highcharts/modules/annotations-8.0.4.js"></script>
+		<?php if ($graphType == 'heatmap') : ?>
+			<script src="vendors/highcharts/modules/boost-8.0.4.js"></script>
+		<?php endif; ?>
 		<?php endif; ?>
 		<script src="https://code.highcharts.com/js/highcharts-more.js"></script>
 		<script src="https://code.highcharts.com/modules/exporting.js"></script>
