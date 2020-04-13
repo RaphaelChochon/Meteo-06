e5510b897447a3f614f6fd6143e907fd0ab2b5d9
Raphaël CHOCHON <raphael@chochon.fr>
2020-04-13 18:17:12 +0200

    Migration vers Bootstrap 4 | Part 3 (webcam)

--- webcam.php
+++ webcam.php
@@ -5,7 +5,7 @@
 		<title><?php echo $short_station_name; ?> | Webcam</title>
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 		<!-- Balises META SEO pour le referencement Google, Facebook Twitter etc. -->
 		<meta name="description" content="Webcam en live de la station <?php echo $station_name; ?> <?php echo $hashtag_meteo; ?>"/>
 		<link rel="canonical" href="<?php if ($SSL){echo'https://';}else echo'http://';?><?php echo $_SERVER['HTTP_HOST']; ?><?php echo $_SERVER['PHP_SELF']; ?>" />
@@ -34,99 +34,92 @@
 			<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 			<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 		<![endif]-->
-		<link href="vendors/bootswatch-flatly/bootstrap.css" rel="stylesheet">
-		<link href="vendors/custom/custom.css?v=1.1" rel="stylesheet">
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-		<script src="vendors/bootstrap/js/bootstrap.js"></script>
+		<!-- JQUERY JS -->
+		<script src="vendors/jquery/jquery-slim-3.4.1.min.js"></script>
+
+		<!-- Bootstrap 4.4.1 -->
+		<link href="vendors/bootstrap/css/bootswatch-united-4.4.1.min.css" rel="stylesheet">
+		<link href="vendors/custom/custom.css?v=1.2" rel="stylesheet">
+		<script src="vendors/bootstrap/js/popper-1.16.0.min.js"></script>
+		<script src="vendors/bootstrap/js/bootstrap-4.4.1.min.js"></script>
 	</head>
 	<body>
 	<div class="container">
 		<header>
 			<?php include 'header.php';?>
 		</header>
-	<br>
+		<br>
 		<nav>
 			<?php include 'nav.php';?>
 		</nav>
 
 		<!-- DEBUT DU CORPS DE PAGE -->
+		<br>
 		<div class="row">
-			<div class="col-md-12 divCenter">
-				<div class="panel panel-info">
-					<div class="panel-heading">
-						<h3 class="panel-title">Webcam <?php echo $station_name; ?></h3>
+			<div class="col-md-12">
+				<!-- Webcam(s) -->
+				<div class="card text-center">
+					<div class="card-header">
+						<h3>Webcam <?php echo $station_name; ?></h3>
 					</div>
-					<div class="panel-body">
-						<div class="row">
-							<div class="col-md-12 divCenter">
-								<h4><?php echo $webcam_view_1; ?></h4>
-								<div class="thumbnail"><img id="webcam_last" src="<?php echo $webcam_url_1; ?>" alt="<?php echo $station_name; ?>"></div>
-								<p>Image rafraichie toutes les <?php echo $webcam_refresh_1; ?> minutes</p>
-								<!--
-									IF PRESENCE_SECOND_WEBCAM TRUE
-								-->
-								<?php if ($presence_second_webcam) : ?>
-								<hr>
-								<h4><?php echo $webcam_view_2; ?></h4>
-								<div class="thumbnail"><img id="webcam_last" src="<?php echo $webcam_url_2; ?>" alt="<?php echo $station_name; ?>"></div>
-								<p>Image rafraichie toutes les <?php echo $webcam_refresh_2; ?> minutes</p>
-								<?php endif; ?>
-								<!--
-									END IF
-								-->
-							</div>
+					<div class="card-body">
+						<h5 class="card-title"><?php echo $webcam_view_1; ?></h5>
+						<div class="img-thumbnail text-center">
+							<img class="img-fluid" src="<?php echo $webcam_url_1; ?>" alt="Dernière image de la webcam de <?php echo $station_name; ?>">
 						</div>
+						<!-- IF deuxième webcam -->
+						<?php if ($presence_second_webcam) : ?>
+						<hr>
+						<h5 class="card-title"><?php echo $webcam_view_2; ?></h5>
+						<div class="img-thumbnail text-center">
+							<img class="img-fluid" src="<?php echo $webcam_url_2; ?>" alt="Dernière image de la webcam de <?php echo $station_name; ?>">
+						</div>
+						<?php endif; ?>
+						<!-- FIN deuxième webcam -->
+					</div>
+					<div class="card-footer text-muted">
+						Image rafraichie toutes les <?php echo $webcam_refresh_1; ?> minutes
 					</div>
 				</div>
-				<div class="panel panel-info">
-					<div class="panel-heading">
-						<h3 class="panel-title">Timelapse de la veille</h3>
+				<br>
+				<!-- Timelapse(s) -->
+				<div class="card text-center">
+					<div class="card-header">
+						<h3>Timelapse de la veille</h3>
 					</div>
-					<div class="panel-body">
-						<div class="row">
-							<div class="col-md-12 divCenter">
-								<video controls width="100%" preload="metadata" poster="<?php echo $timelapse_poster_url_1; ?>">
+					<div class="card-body">
+						<h5 class="card-title"><?php echo $webcam_view_1; ?></h5>
+						<div class="img-thumbnail text-center">
+							<video controls width="100%" preload="metadata" poster="<?php echo $timelapse_poster_url_1; ?>">
 								<source src="<?php echo $timelapse_url_1; ?>" type="video/webm" />
-								</video>
-							<p>Ce timelapse est réalisé tous les soirs (vers 22h30 environ), à partir des images de la webcam collectées toute la journée (soit une image toutes les <?php echo $webcam_refresh_1; ?> minutes).</p>
-							</div>
-
-						<?php if ($presence_archive_timelapse) : ?>
-							<div class="col-md-6 col-md-offset-3 divCenter">
-								<a href="<?php echo $timelapse_archive_url_1; ?>" target="blank" class="btn btn-default btn-lg btn-block">Accéder aux archives ici</a>
-								<br>
-							</div>
-						<?php endif; ?>
+							</video>
 						</div>
-					<!--
-						IF PRESENCE_SECOND_TIMELAPSE TRUE
-					-->
-					<?php if ($presence_second_timelapse) : ?>
-					<div class="row">
-							<div class="col-md-12 divCenter">
-							<hr>
-								<video controls width="100%" preload="metadata" poster="<?php echo $timelapse_poster_url_2; ?>">
+						<br>
+						<a role="button" class="btn btn-primary" href="<?php echo $timelapse_archive_url_1; ?>" target="_blank">Consulter les archives</a>
+						<!-- IF deuxième timelapse -->
+						<?php if ($presence_second_timelapse) : ?>
+						<hr>
+						<h5 class="card-title"><?php echo $webcam_view_2; ?></h5>
+						<div class="img-thumbnail text-center">
+							<video controls width="100%" preload="metadata" poster="<?php echo $timelapse_poster_url_2; ?>">
 								<source src="<?php echo $timelapse_url_2; ?>" type="video/webm" />
-								</video>
-							<p>Ce timelapse est réalisé tous les soirs (vers 22h30 environ), à partir des images de la webcam collectées toute la journée (soit une image toutes les <?php echo $webcam_refresh_2; ?> minutes).</p>
-							</div>
-							<?php if ($presence_archive_timelapse) : ?>
-								<div class="col-md-6 col-md-offset-3 divCenter">
-									<a href="<?php echo $timelapse_archive_url_2; ?>" target="blank" class="btn btn-default btn-lg btn-block">Accéder aux archives ici</a>
-								</div>
-							<?php endif; ?>
+							</video>
+						</div>
+						<br>
+						<a role="button" class="btn btn-primary" href="<?php echo $timelapse_archive_url_2; ?>" target="_blank">Consulter les archives</a>
+						<?php endif; ?>
+						<!-- FIN deuxième timelapse -->
 					</div>
-					<?php endif; ?>
-					<!--
-						END IF
-					-->
+					<div class="card-footer text-muted">
+						Ce timelapse est réalisé tous les soirs (vers 22h30 environ), à partir des images de la webcam collectées toute la journée.
 					</div>
 				</div>
 			</div>
 		</div>
+		<br>
 
-	<footer>
-		<?php include 'foot.php';?>
+	<footer class="footer bg-light">
+		<?php include 'footer.php';?>
 	</footer>
 	</div>
 	</body>
