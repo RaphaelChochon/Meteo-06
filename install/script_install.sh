d4416ad57c94057af93e5a54d262db70e899e3f2
Raphaël CHOCHON <raphael@chochon.fr>
2020-04-30 10:09:29 +0200

    Update script_install.sh

--- install/script_install.sh
+++ install/script_install.sh
@@ -22,14 +22,11 @@ if confirm "Nous allons reconfigurer le site en vous demandant au cas par cas si
 	echo "Ok nous allons commencer :"
 	cd ../config/
 	cp -iv config-SAMPLE.php config.php
-	cp -iv json_cron-SAMPLE.sh json_cron.sh
-	cp -iv json_archives_cron-SAMPLE.sh json_archives_cron.sh
 	cp -iv web_analytics-SAMPLE.php web_analytics.php
 	cp -iv additional_menu-SAMPLE.php additional_menu.php
 	cp -iv res_sociaux-SAMPLE.php res_sociaux.php
 	cp -iv widget_vigi-SAMPLE.php widget_vigi.php
 	cp -iv a-propos-SAMPLE.php a-propos.php
-	chmod +x json_cron.sh json_archives_cron.sh
 	cd ../img/
 	cp -iv logo-SAMPLE.jpg logo.jpg
 else
