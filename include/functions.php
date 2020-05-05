ece27b4f35ddad2c4706c44e7cedfbfe3e016d6f
Raphaël CHOCHON <raphael@chochon.fr>
2020-05-05 14:33:40 +0200

    Update functions.php

--- include/functions.php
+++ include/functions.php
@@ -51,22 +51,22 @@
 // Position cardinale du vent (en texte plutôt qu'en degrés)
 	function wind_cardinals($deg) {
 		$cardinalDirections = array(
-			'N' => array(348.75, 361),
-			'N2' => array(0, 11.25),
+			'N'   => array(348.75, 361),
+			'N'   => array(0, 11.25),
 			'NNE' => array(11.25, 33.75),
-			'NE' => array(33.75, 56.25),
+			'NE'  => array(33.75, 56.25),
 			'ENE' => array(56.25, 78.75),
-			'E' => array(78.75, 101.25),
+			'E'   => array(78.75, 101.25),
 			'ESE' => array(101.25, 123.75),
-			'SE' => array(123.75, 146.25),
+			'SE'  => array(123.75, 146.25),
 			'SSE' => array(146.25, 168.75),
-			'S' => array(168.75, 191.25),
+			'S'   => array(168.75, 191.25),
 			'SSW' => array(191.25, 213.75),
-			'SW' => array(213.75, 236.25),
+			'SW'  => array(213.75, 236.25),
 			'WSW' => array(236.25, 258.75),
-			'W' => array(258.75, 281.25),
+			'W'   => array(258.75, 281.25),
 			'WNW' => array(281.25, 303.75),
-			'NW' => array(303.75, 326.25),
+			'NW'  => array(303.75, 326.25),
 			'NNW' => array(326.25, 348.75)
 		);
 		foreach ($cardinalDirections as $dir => $angles) {
