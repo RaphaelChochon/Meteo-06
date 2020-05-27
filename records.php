fdf22a64120fa8b63cd9e45059ac4104016e735f
Raphaël CHOCHON <raphael@chochon.fr>
2020-05-27 15:40:46 +0200

    Update records.php

--- records.php
+++ records.php
@@ -117,13 +117,23 @@
 			<?php if (!isset($_GET['more'])) :?>
 				<div class="row mb-3">
 					<div class="col text-center">
-						<a role="button" class="btn btn-primary" href="./records.php?more"><i class="fas fa-chevron-circle-left"></i>&nbsp;Charger plus de records ?</a>
+						<a role="button" class="btn btn-primary" href="./records.php?more">
+							<svg class="bi bi-plus-circle" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+								<path fill-rule="evenodd" d="M8 3.5a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-.5.5H4a.5.5 0 0 1 0-1h3.5V4a.5.5 0 0 1 .5-.5z"/>
+								<path fill-rule="evenodd" d="M7.5 8a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1H8.5V12a.5.5 0 0 1-1 0V8z"/>
+								<path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+							</svg>
+							&nbsp;
+							Charger plus de records ?
+						</a>
 					</div>
 				</div>
 			<?php else : ?>
 				<div class="row mb-3">
 					<div class="col text-center">
-						<a role="button" class="btn btn-primary" href="./records.php"><i class="fas fa-chevron-circle-left"></i>&nbsp;Charger moins de records ?</a>
+						<a role="button" class="btn btn-primary" href="./records.php">
+							Charger moins de records ?
+						</a>
 					</div>
 				</div>
 			<?php endif ; ?>
