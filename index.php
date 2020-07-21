0b335c8fecc071d72a518d648e55546194ae12dd
Raphaël CHOCHON <raphael@chochon.fr>
2020-07-21 16:34:17 +0200

    Update index.php
    Fix typo "max" inutile pour le tableau UV et rayonnement solaire

--- index.php
+++ index.php
@@ -352,7 +352,7 @@
 						<tbody>
 							<tr>
 								<th>
-									UV max
+									UV
 									<span class="float-right">
 										<svg class="bi bi-info-circle" width="0.7em" height="0.7em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg" data-toggle="tooltip" data-placement="top" data-html="true" title="<u>Indice UV</u> maximale de la journée, entre 00h UTC et 23h59 UTC inclus">
 											<path fill-rule="evenodd" d="M8 15A7 7 0 108 1a7 7 0 000 14zm0 1A8 8 0 108 0a8 8 0 000 16z" clip-rule="evenodd"/>
@@ -379,7 +379,7 @@
 							</tr>
 							<tr>
 								<th>
-									Ray. sol. max
+									Ray. sol.
 									<span class="float-right">
 										<svg class="bi bi-info-circle" width="0.7em" height="0.7em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg" data-toggle="tooltip" data-placement="top" data-html="true" title="Le <u>rayonnement solaire</u> maximale de la journée, entre 00h UTC et 23h59 UTC inclus">
 											<path fill-rule="evenodd" d="M8 15A7 7 0 108 1a7 7 0 000 14zm0 1A8 8 0 108 0a8 8 0 000 16z" clip-rule="evenodd"/>
